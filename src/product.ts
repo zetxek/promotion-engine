9bcd9db8bcc31d06afa2caed4251313c639c26e6
Adrián Moreno Peña <zetxek@gmail.com>
2021-01-23 17:41:24 +0100

    [PM-6] Add product class

---
+++ src/product.ts
@@ -0,0 +1,12 @@
+/**
+ * Holds information about a product: SKU and price
+ */
+export class Product {
+    public sku : string;
+    public price: number;
+
+    constructor(sku: string, price: number){
+        this.sku = sku;
+        this.price = price;
+    }
+}

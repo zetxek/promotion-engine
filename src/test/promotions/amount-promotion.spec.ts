3bff41d6d089f747786d594e8c4ccc8f6d0127ab
Adrián Moreno Peña <zetxek@gmail.com>
2021-01-23 19:48:04 +0100

    [PM-6] Add test for promotion 2

--- src/test/promotions/amount-promotion.spec.ts
+++ src/test/promotions/amount-promotion.spec.ts
@@ -8,12 +8,22 @@ import { AmountPromotion } from "../../promotions/amount-promotion";
 describe("AmountPromotion", () => { 
 
     describe("AmountPromotion(A,3,130)", () => {
-        it("should create a", () => {
+        
+        it("should get a discount of 20", () => {
             const pA = new Product("A", 50);
             const promotion = new AmountPromotion(pA, 3, 130);
             const cart = new Cart();
             cart.add(pA, 3);
             promotion.calculateDiscount(cart).should.be.equal(20);
         });
+        
+        it("should get a discount of 20", () => {
+            const pB = new Product("B", 30);
+            const promotion = new AmountPromotion(pB, 2, 45);
+            const cart = new Cart();
+            cart.add(pB, 2);
+            promotion.calculateDiscount(cart).should.be.equal(15);
+        });
+        
     });
 });

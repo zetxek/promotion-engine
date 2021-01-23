de56fec7f0bc6d0c36172ffd5499e33dda6a55bb
Adrián Moreno Peña <zetxek@gmail.com>
2021-01-23 17:13:34 +0100

    [PM-3] Setup basic test
    And the testing runners

---
+++ src/test/hello-world.spec.ts
@@ -0,0 +1,26 @@
+import "mocha"
+import "should"
+
+import { HelloWorld } from "../hello-world";
+
+describe("HelloWorld", () => { 
+
+    let tested: HelloWorld;
+
+    beforeEach(() => tested = new HelloWorld());
+
+    describe("Say hi, ()", () => {
+        it("should say Hello, adrian", () => {
+            const result = tested.sayHello("adrian");
+            const expected = "Hello, adrian";
+            result.should.be.equal(expected);
+        });
+
+        it("should say Hello, world", () => {
+            const result = tested.sayHello("world");
+            const expected = "Hello, world";
+            result.should.be.equal(expected);
+        })
+    });
+
+});
